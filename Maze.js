bbd5e4be31100f55163abfff743c6db119ee9855
trbabalh <huseyin.babal@eu.sony.com>
2014-07-11 17:28:15 +0300

    draw added

--- Maze.js
+++ Maze.js
@@ -6,9 +6,14 @@ var Maze = {
     startY: null,
     endX: null,
     endY: null,
+    possibleStartEndPoints: [],
     initMazeObject: function(n) {
         for(var i = 0; i < n; i++) {
             Maze.mazeCoords[i] = {};
+        }
+    },
+    initMazeVisited: function(n) {
+        for(var i = 0; i < n; i++) {
             Maze.visited[i] = {};
         }
     },
@@ -20,7 +25,7 @@ var Maze = {
             for (var i = 0; i < n; i++) {
                 html += '<tr>';
                 for (var j = 0; j < n; j++) {
-                    html += '<td class="cell ' + Maze.calculateClass(mazeArr[i][j]) + '" data-x="' + i + '" data-y="' + j + '" title="' + Number(mazeArr[i][j]).toString(2) + '"></td>';
+                    html += '<td class="cell ' + Maze.calculateClass(mazeArr[i][j]) + '" data-x="' + i + '" data-y="' + j + '"></td>';
                     Maze.mazeCoords[i][j] = Maze.pad(Number(mazeArr[i][j]).toString(2)).split('');
                 }
                 html += '</tr>';
@@ -28,42 +33,133 @@ var Maze = {
             html += '</table>';
             jQuery("#mazeArea").html(html);
             jQuery.unblockUI();
+            Maze.findPossibleStartEndPoints();
         });
 
     },
     findPossibleStartEndPoints: function() {
-
+        var mazeLength = Maze.mazeCoords.length;
+        for (var i = 0; i < mazeLength; i++) {
+            for (var j = 0; j < mazeLength; j++) {
+                var binaryArr = Maze.mazeCoords[i][j];
+                if (i == 0) {
+                    if (j == 0) {
+                        if (binaryArr[0] == 1 || binaryArr[3] == 1) {
+                            Maze.possibleStartEndPoints.push([i, j]);
+                            Maze.addPointerToCell(i, j);
+                        }
+                    } else if (j == mazeLength - 1) {
+                        if (binaryArr[1] == 1 || binaryArr[3] == 1) {
+                            Maze.possibleStartEndPoints.push([i, j]);
+                            Maze.addPointerToCell(i, j);
+                        }
+                    } else {
+                        if (binaryArr[3] == 1) {
+                            Maze.possibleStartEndPoints.push([i, j]);
+                            Maze.addPointerToCell(i, j);
+                        }
+                    }
+                } else if (i == mazeLength - 1) {
+                    if (j == 0) {
+                        if (binaryArr[0] == 1 || binaryArr[2] == 1) {
+                            Maze.possibleStartEndPoints.push([i, j]);
+                            Maze.addPointerToCell(i, j);
+                        }
+                    } else if (j == mazeLength - 1) {
+                        if (binaryArr[1] == 1 || binaryArr[2] == 1) {
+                            Maze.possibleStartEndPoints.push([i, j]);
+                            Maze.addPointerToCell(i, j);
+                        }
+                    } else {
+                        if (binaryArr[2] == 1) {
+                            Maze.possibleStartEndPoints.push([i, j]);
+                            Maze.addPointerToCell(i, j);
+                        }
+                    }
+                } else {
+                    if (j == 0) {
+                        if (binaryArr[0] == 1) {
+                            Maze.possibleStartEndPoints.push([i, j]);
+                            Maze.addPointerToCell(i, j);
+                        }
+                    } else if (j == mazeLength - 1) {
+                        if (binaryArr[1] == 1) {
+                            Maze.possibleStartEndPoints.push([i, j]);
+                            Maze.addPointerToCell(i, j);
+                        }
+                    }
+                }
+            }
+        }
+    },
+    addPointerToCell: function(x, y) {
+        var currentSize = jQuery('.badge').length;
+        jQuery('[data-x="' + x + '"][data-y="' + y + '"]')
+            .on("mouseenter", function() {
+                jQuery(this).css({'background-color': '#5bc0de'});
+            }).on("mouseleave", function() {
+                jQuery(this).css({'background-color': '#EEEEEE'});
+            }).css({'cursor': 'pointer'});
+        jQuery('<span>').appendTo('[data-x="' + x + '"][data-y="' + y + '"]')
+            .html(currentSize + 1).attr("class", "badge").css({
+            'background-color': '#428bca',
+            'margin': 'auto'
+        });
+    },
+    startSolve: function() {
+        for (var i in Maze.possibleStartEndPoints) {
+            for (var j in Maze.possibleStartEndPoints) {
+                if (i != j) {
+                    Maze.initMazeVisited(Maze.mazeCoords.length);
+                    Maze.startX = Maze.possibleStartEndPoints[i][0];
+                    Maze.startY = Maze.possibleStartEndPoints[i][1];
+                    Maze.endX = Maze.possibleStartEndPoints[j][0];
+                    Maze.endY = Maze.possibleStartEndPoints[j][1];
+                    var result = Maze.solveMaze(Maze.startX, Maze.startY);
+                    if (result) {
+                        var pStart = '<label class="label label-success">' + jQuery('[data-x="' + Maze.startX + '"][data-y="' + Maze.startY + '"]').text() + '</label>';
+                        var pEnd = '<label class="label label-success">' + jQuery('[data-x="' + Maze.endX + '"][data-y="' + Maze.endY + '"]').text() + '</label>';
+                        var key = "" + Maze.startX + "" + Maze.startY + "" + Maze.endX + "" + Maze.endY;
+                        jQuery("#results tbody").append('<tr><td>' + pStart + '</td><td>' + pEnd + '</td><td><button type="button" class="btn btn-primary show-result" data-index="' + key + '"><span class="glyphicon glyphicon-pencil"></span> Show</button></td></tr>');
+                    }
+                }
+            }
+        }
+        jQuery(".show-result").on("click", function() {
+            Maze.drawResult(jQuery(this).data("index"));
+        });
     },
     solveMaze: function(x, y) {
-        if (x == this.endX && y == this.endY) return true;
-        if (this.visited[x][y]) return false;
-        this.visited[x][y] = true;
-
-        if (this.mazeCoords[x][y][3] ) {
+        var mazeLength = Maze.mazeCoords.length;
+        if (x > mazeLength - 1 || x < 0 || y > mazeLength - 1 || y < 0) return false;
+        if (x == Maze.endX && y == Maze.endY) return true;
+        if (Maze.visited[x][y]) return false;
+        Maze.visited[x][y] = true;
+        if (Maze.mazeCoords[x][y][3] == 1) {
             // Check top
-            if (this.solveMaze(x, y + 1)) {
-                console.log([x, y+1]);
+            if (Maze.solveMaze(x - 1, y)) {
+                Maze.addToCorrectPath(Maze.startX, Maze.startY, Maze.endX, Maze.endY, x - 1, y);
                 return true;
             }
         }
-        if (this.mazeCoords[x][y][1]) {
+        if (Maze.mazeCoords[x][y][1] == 1) {
             // Check right
-            if (this.solveMaze(x + 1, y)) {
-                console.log([x+1, y]);
+            if (Maze.solveMaze(x, y + 1)) {
+                Maze.addToCorrectPath(Maze.startX, Maze.startY, Maze.endX, Maze.endY, x, y + 1);
                 return true;
             }
         }
-        if (this.mazeCoords[x][y][2]) {
+        if (Maze.mazeCoords[x][y][2] == 1) {
             // Check bottom
-            if (this.solveMaze(x, y - 1)) {
-                console.log([x, y-1]);
+            if (Maze.solveMaze(x + 1, y)) {
+                Maze.addToCorrectPath(Maze.startX, Maze.startY, Maze.endX, Maze.endY, x + 1, y + 1);
                 return true;
             }
         }
-        if (this.mazeCoords[x][y][0]) {
+        if (Maze.mazeCoords[x][y][0] == 1) {
             // Check left
-            if (this.solveMaze(x - 1, y)) {
-                console.log([x-1, y]);
+            if (Maze.solveMaze(x, y - 1)) {
+                Maze.addToCorrectPath(Maze.startX, Maze.startY, Maze.endX, Maze.endY, x, y - 1);
                 return true;
             }
         }
@@ -83,14 +179,60 @@ var Maze = {
         while (s.length < 4) s = "0" + s;
         return s;
     },
+    addToCorrectPath: function(startX, startY, endX, endY, x, y) {
+        var key = "" + startX + "" + startY + "" + endX + "" + endY;
+        if (typeof Maze.correctPath[key] != 'undefined') {
+            Maze.correctPath[key].push([x, y]);
+        } else {
+            Maze.correctPath[key] = [[x, y]];
+        }
+    },
+    drawResult: function(key) {
+        var paper = Raphael(0, 0, 1200, 1200);
+        var startX = null;
+        var startY = null;
+        var endX = null;
+        var endY = null;
+        for (var i = 0; i < Maze.correctPath[key].length; i++) {
+            var center = Maze.getCenterCoordinates(Maze.correctPath[key][i][0], Maze.correctPath[key][i][1]);
+            if (i == 0) {
+                startX = center[0];
+                startY = center[1];
+                continue;
+            } else if (i == 1) {
+                endX = center[0];
+                endY = center[1];
+            } else {
+                startX = endX;
+                startY = endY;
+                endX = center[0];
+                endY = center[1];
+            }
+            var line = paper.path( "M" + startX + " " + startY );
+            line.animate({path: [["M", startX, startY], ["L", endX, endY]], "stroke-width": 2}, 3000);
+        }
+    },
+    getCenterCoordinates: function(x, y) {
+        var cell = jQuery('[data-x="' + x + '"][data-y="' + y + '"]');
+        var offset = cell.offset();
+        var width = cell.width();
+        var height = cell.height();
+
+        var centerX = offset.left + width / 2;
+        var centerY = offset.top + height / 2;
+        return [centerX, centerY];
+    },
     init: function() {
         Maze.initMazeObject(15);
+        Maze.initMazeVisited(15);
         Maze.initMazeArea(15);
         jQuery("#generate").on("click", function() {
             Maze.initMazeArea(15);
         });
-        //Maze.solveMaze(this.startX, this.startY);
-        //console.log(this.correctPath);
+        jQuery("#solve").on("click", function() {
+            Maze.startSolve();
+        });
+
     }
 
 }
